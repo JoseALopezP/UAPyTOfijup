6a107d9ce3ecc6646811fdbcb2f84c48a0e606d1
JoseALopezP <jalp42@live.com>
2024-04-03 13:26:24 -0300

    Added structure for table

---
+++ src/app/components/ScheduleTable.jsx
@@ -0,0 +1,9 @@
+import styles from './ScheduleTable.module.css'
+
+export function ScheduleTable () {
+    return(
+        <section className={`${styles.navSection}`}>
+
+        </section>
+    )
+}
