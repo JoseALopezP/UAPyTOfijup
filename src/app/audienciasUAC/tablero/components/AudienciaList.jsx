675f3fda9c7b97ccb93a9395bc1113f0e715be28
JoseALopezP <jalp42@live.com>
2024-05-01 12:18:19 -0300

    Finished developing spree, it's my day

--- src/app/audienciasUAC/tablero/components/AudienciaList.jsx
+++ src/app/audienciasUAC/tablero/components/AudienciaList.jsx
@@ -18,28 +18,28 @@ export function AudienciaList ({date}) {
             <table className={`${styles.table}`} cellSpacing="0" cellPadding="0">
                 <thead className={`${styles.tableHead}`}>
                     <tr>
-                        <th>ADMIN</th>
-                        <th>LEGAJO</th>
-                        <th>TIPO DE AUDIENCIA</th>
-                        <th>JUEZ</th>
-                        <th>NATURAL</th>
-                        <th>SITUACIÓN</th>
-                        <th>RESULTADO</th>
-                        <th>FINALIZADA</th>
+                        <th className={`${styles.tableHeadCell} ${styles.tableCellAdmin}`}>ADMIN</th>
+                        <th className={`${styles.tableHeadCell} ${styles.tableCellLeg}`}>LEGAJO</th>
+                        <th className={`${styles.tableHeadCell} ${styles.tableCellTipo}`}>TIPO DE AUDIENCIA</th>
+                        <th className={`${styles.tableHeadCell} ${styles.tableCellJuez}`}>JUEZ</th>
+                        <th className={`${styles.tableHeadCell} ${styles.tableCellJuezN}`}>NATURAL</th>
+                        <th className={`${styles.tableHeadCell} ${styles.tableCellSituacion}`}>SITUACIÓN</th>
+                        <th className={`${styles.tableHeadCell} ${styles.tableCellResultado}`}>RESULTADO</th>
+                        <th className={`${styles.tableHeadCell} ${styles.tableCellEstado}`}>FINALIZADA</th>
                     </tr>
                 </thead>
                 <tbody className={`${styles.tableBody}`}>
                     {today && today.sort((a,b)=>(a.hora.split(':').join('') - b.hora.split(':').join(''))).map((el)=>{
                         return(
                             <tr key={el.numeroLeg + el.hora} className={el.estado == 'FINALIZADA' ? `${styles.tableRow} ${styles.tableRowFinalizada}` : `${styles.tableRow}`}> 
-                                <td>{el.admin && el.admin}</td>
-                                <td>{el.numeroLeg}</td>
+                                <td className={`${styles.tableCellAdmin}`}>{el.admin && el.admin}</td>
+                                <td className={`${styles.tableCellLeg}`}>{el.numeroLeg}</td>
                                 <td className={`${styles.tableCellTipo}`}>{el.tipo}</td>
-                                <td>{el.juez.split('+').map(e => <span key={e}>{e}<br/></span>)}</td>
+                                <td className={`${styles.tableCellJuez}`}>{el.juez.split('+').map(e => <span key={e}>{e}<br/></span>)}</td>
                                 <td className={`${styles.tableCellJuezN}`}>{el.juezN && el.juezN.split(' ').map((word, i) => (i == 0) ?  `${word}` + ' ' : word.substring(0, 1))}</td>
-                                <td>{el.situacion && el.situacion}</td>
-                                <td>{el.resultado && el.resultado}</td>
-                                <td>{(el.hito && el.estado == "FINALIZADA") && el.hito[el.hito.length() - 1].split('|')[0]}</td>
+                                <td className={`${styles.tableCellSituacion}`}>{el.situacion && el.situacion}</td>
+                                <td className={`${styles.tableCellResultado}`}>{el.resultado && el.resultado}</td>
+                                <td className={`${styles.tableCellEstado}`}>{(el.hitos && el.estado == "FINALIZADA") && el.hitos[el.hitos.length - 1].split('|')[0]}</td>
                             </tr>
                         )
                     })}
