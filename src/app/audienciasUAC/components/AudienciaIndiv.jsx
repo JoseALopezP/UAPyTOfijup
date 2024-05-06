2925f4017f46c727b1c180255f9ad3dbb642b2c7
José López <jalp42@live.com>
2024-05-06 10:18:38 -0300

    Changed spaces between audiencia types

--- src/app/audienciasUAC/components/AudienciaIndiv.jsx
+++ src/app/audienciasUAC/components/AudienciaIndiv.jsx
@@ -77,7 +77,7 @@ export function AudienciaIndiv ({date, element}) {
                 </>}
             </span>
             <span className={`${styles.tableCell} ${styles.tableCellLegajo} ${styles.tableCellLegajoIndiv}`}>{element.numeroLeg}</span>
-            <span className={`${styles.tableCell} ${styles.tableCellTipoIndiv}`}>{element.tipo}{element.tipo2 && '+ ' + element.tipo2}</span>
+            <span className={`${styles.tableCell} ${styles.tableCellTipoIndiv}`}>{element.tipo}{element.tipo2 && ' + ' + element.tipo2}</span>
             <span className={`${styles.tableCell} ${styles.tableCellJuez} ${styles.tableCellJuezList}`}>{element.juez.split('+').map(e => <span key={e}>{e.split(' ').slice(1,3).join(' ')}<br/></span>)}</span>
             <span className={`${styles.tableCell} ${styles.tableCellJuezN}`}>
                 <select onChange={(e)=>{setJuezN(e.target.value)}}>
