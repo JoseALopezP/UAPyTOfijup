f68c826f739dcc2959e4cac299032c58f1ef6a65
José López <jalp42@live.com>
2024-05-16 13:32:57 -0300

    Started informacion adding structure

--- src/app/informacion/components/BlockCarga.jsx
+++ src/app/informacion/components/BlockCarga.jsx
@@ -3,7 +3,7 @@ import styles from './informacion.module.css'
 export function BlockCarga () {
     return(
         <section className={`${styles.cargaSection}`}>
-
+            <div>Agregar Información</div>
         </section>
     )
 }
