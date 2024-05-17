96160989f2bffb7465e06d93a6ae5e8673b2e76a
José López <jalp42@live.com>
2024-05-17 13:28:53 -0300

    Added structure for inputs in informacion

--- src/app/informacion/components/BlockCarga.jsx
+++ src/app/informacion/components/BlockCarga.jsx
@@ -3,7 +3,17 @@ import styles from './informacion.module.css'
 export function BlockCarga () {
     return(
         <section className={`${styles.cargaSection}`}>
-            <div>Agregar Información</div>
+            <div className={`${styles.inputDiv}`}>
+            <h2>AGREGAR UN NUEVO DOCUMENTO</h2>
+            <form action="" className={`${styles.inputForm}`}>
+                <label>TÍTULO</label>
+                <input type="text" />
+                <label>CUERPO</label>
+                <label>IMÁGEN</label>
+                <label>FECHA INICIO</label>
+                <label>FECHA FIN</label>
+            </form>
+            </div>
         </section>
     )
 }
--- src/app/informacion/components/BlockList.jsx
+++ src/app/informacion/components/BlockList.jsx
@@ -3,7 +3,9 @@ import styles from './informacion.module.css'
 export function BlockList () {
     return(
         <section className={`${styles.listSection}`}>
-
+            <span>
+                <img src='https://www.google.com/url?sa=i&url=https%3A%2F%2Fdipascuale.com%2Fclientes%2Flapana%2F&psig=AOvVaw07jnn9W7XHh5dW5QuotcGW&ust=1716049395415000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCICz4qiMlYYDFQAAAAAdAAAAABAE'/>
+            </span>
         </section>
     )
 }
