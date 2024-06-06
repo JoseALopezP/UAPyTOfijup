e4280cc7274a6fdaea6492e98dcab5e5f5604a54
José López <jalp42@live.com>
2024-06-06 13:35:17 -0300

    Finished resuelvo and minuta to pdf

--- src/utils/pdfUtils.js
+++ src/utils/pdfUtils.js
@@ -1,6 +1,4 @@
 import { jsPDF } from 'jspdf';
-import html2canvas from 'html2canvas';
-import { useRef } from 'react';
 
 export const generatePDF = async () => {
     const doc = new jsPDF('p', 'mm', 'a4');
@@ -19,40 +17,68 @@ export const generatePDF = async () => {
       { title: "Imputado", text: "Rodolfo Duilio, Rosales. D.N.I N°: 13.446.438." },
       { title: "Fecha de detención", text: "26/04/2024, a las 08:30 horas en la División Delitos Central de Policía." },
       { title: "Operador", text: "Nahuel Sosa." },
+      { text: "" },
       { text: "El imputado ratifica la designación de sus abogado defensor y el mismo acepta el cargo. (Minuto 00:01:00 Video 1)." },
-      { text: "Ministerio Público Fiscal: (Minuto 00:01:10 Video 1). Realiza el control de legalidad de la detención." },
-      { text: "Defensa Técnica: (Minuto 00:04:15 Video 1). No se opone a la legalidad de la detención del imputado. SE DECLARA la legalidad de la detención del imputado. (Minuto 00:04:50 Video 1)." },
-      { text: "Ministerio Público Fiscal: (Minuto 00:05:05 Video 1). Refiere el hecho que diera inicio a las actuaciones, consignando las constancias que lo acreditan, ofreciendo la prueba pertinente, calificándolo provisoriamente como constitutivo del delito de “LIBRAMIENTO DE CHEQUE SIN PROVISIÓN DE FONDOS” (ART. 302 INC 2° DEL C.P.) EN CALIDAD DE AUTOR MATERIAL (ART. 45 DEL C.P.), Y EN PERJUICIO DEL CIUDADANO FLORES ROBERTO CEFERINO Y “ESTAFA” (ARTS. 172 Y 296 EN FUNCIÓN DEL ART. 285 DEL C.P.), EN PERJUICIO DEL CIUDADANO BAZÁN MARIO FEDERICO EN CONCURSO IDEAL (ART. 54 C.P.), EN CALIDAD DE AUTOR MATERIAL (ART. 45 DEL C.P.), TODO ELLO EN CONCURSO REAL (ART. 55 C.P.). Solicita como plazo de la I.P.P. el de DIEZ (10) MESES. Solicita como medidas de coerción las establecidas en el Art. 295 inc. 1, 3, 4, 5 y 6 por el plazo de DIEZ (10) MESES." },
+      { text: "Ministerio Público Fiscal: (Minuto 00:01:10 Video 1).\n Realiza el control de legalidad de la detención." },
+      { text: "Defensa Técnica: (Minuto 00:04:15 Video 1).\n No se opone a la legalidad de la detención del imputado. SE DECLARA la legalidad de la detención del imputado. (Minuto 00:04:50 Video 1)." },
+      { text: "Ministerio Público Fiscal: (Minuto 00:05:05 Video 1).\n Refiere el hecho que diera inicio a las actuaciones, consignando las constancias que lo acreditan, ofreciendo la prueba pertinente, calificándolo provisoriamente como constitutivo del delito de “LIBRAMIENTO DE CHEQUE SIN PROVISIÓN DE FONDOS” (ART. 302 INC 2° DEL C.P.) EN CALIDAD DE AUTOR MATERIAL (ART. 45 DEL C.P.), Y EN PERJUICIO DEL CIUDADANO FLORES ROBERTO CEFERINO Y “ESTAFA” (ARTS. 172 Y 296 EN FUNCIÓN DEL ART. 285 DEL C.P.), EN PERJUICIO DEL CIUDADANO BAZÁN MARIO FEDERICO EN CONCURSO IDEAL (ART. 54 C.P.), EN CALIDAD DE AUTOR MATERIAL (ART. 45 DEL C.P.), TODO ELLO EN CONCURSO REAL (ART. 55 C.P.). Solicita como plazo de la I.P.P. el de DIEZ (10) MESES. Solicita como medidas de coerción las establecidas en el Art. 295 inc. 1, 3, 4, 5 y 6 por el plazo de DIEZ (10) MESES." },
       { text: "Sr. Juez cede la palabra al Imputado en virtud a su derecho de ser oído; ante lo cual se abstiene de declarar. (Minuto 00:18:05 Video 1)." },
       { text: "Defensa Técnica: (Minuto 00:18:10 Video 1). No se opone al plazo de la I.P.P. ni al plazo de la misma. Fundamentos y Resolución: Sr. Juez MOTIVA Y RESUELVE (Minuto 00:23:45 / 00:27:09 Video 1) I) TÉNGASE POR FORMALIZADA la investigación penal preparatoria y por iniciado el proceso y téngase por 10 meses de tiempo para su conclusión, en cuanto a las medidas de coerción se refiere y atento a lo expresado se hace cesar en este momento la detención que pesa sobre el señor Rosales y previo la inmediata libertad le hagamos saber las medidas de coerción en función de lo solicitado: En primer lugar como medida de coerción, la promesa de presentarse a proceso las veces que sea citado, segundo se le pone la obligación de comparecer a la Comisaría Novena, una vez por semana por 10 meses en función de la extensión de la investigación penal preparatoria, en cuanto a el inciso cuarto, si usted va a salir del territorio de la Provincia tiene que hacer saber el motivo, la justificación pertinente para hacer análisis y decidir si se autoriza o no se autoriza la salida de la Provincia incluso por cuánto tiempo, en función de eso y guardando un grado de coherencia sobre el inciso quinto del 295 solicitado por Fiscalía no me dando oposición, reténgase la documentación de viaje pertinente y en cuanto al inciso sexto, se le prohíbe señor Rosales acercarse una distancia inferior a 300 metros de donde quiera que se encuentre y del domicilio respectivo del señor Bazán y en cuanto al depósito judicial se refiere Fiscalía el vehículo está a su disposición hasta que se disponga en forma definitiva y se confiará en depósito Judicial con la documentación pertinente al señor Bazán, que también se le confiará en depósito Judicial del vehículo con la documentación pertinente, para poder transitar permaneciendo el vehículo a disposición de Fiscalía como uno de los elementos secuestrados, téngase por notificado tanto a la Defensa, al señor Rosales como a Fiscalía." }
     ];
 
-    let currentY = 50;
+    const topMargin = 40;
+    const bottomMargin = 40;
+    const pageHeight = 297;
+    let currentY = topMargin;
+    const lineHeight = 7;
+    const sectionSpacingWithTitle = 1;
+    const sectionSpacingWithoutTitle = 6;
 
-    sections.forEach(section => {
+    const addTextWithLineBreaks = (textLines, initialX, initialY) => {
+      textLines.forEach(line => {
+        if (currentY > (pageHeight - bottomMargin)) {
+          doc.addPage();
+          currentY = topMargin;
+        }
+        doc.text(line, initialX, currentY);
+        currentY += lineHeight;
+      });
+    };
+
+    sections.forEach((section) => {
+      let textLines = doc.splitTextToSize(section.text, 160);
+      
       if (section.title) {
+        const title = `${section.title}: `;
+        const titleWidth = doc.getTextWidth(title);
         doc.setFontSize(10);
         doc.setFont("helvetica", "bold");
-        doc.text(`${section.title}: `, 20, currentY);
+        if (currentY > (pageHeight - bottomMargin)) {
+          doc.addPage();
+          currentY = topMargin;
+        }
+        doc.text(title, 20, currentY);
         doc.setFont("helvetica", "normal");
-        doc.text(section.text, (30 + section.title.split('').length * 1.7 - section.title.split(' ').length * 2), currentY);
+        const firstLineText = textLines.shift();
+        doc.text(firstLineText, 25 + titleWidth, currentY);
+        currentY += lineHeight;
+        addTextWithLineBreaks(textLines, 20, currentY);
+        currentY += sectionSpacingWithTitle;
       } else {
         doc.setFontSize(10);
-        doc.text(section.text, 10, currentY);
+        doc.setFont("helvetica", "normal");
+        addTextWithLineBreaks(textLines, 20, currentY);
+        currentY += sectionSpacingWithoutTitle;
       }
-      currentY += 10;
     });
-    // Add headers and footers to each page
     for (let i = 1; i <= doc.internal.getNumberOfPages(); i++) {
         doc.setPage(i);
-        // Add header
-        doc.addImage(headerImage, 'JPEG', 0, 0, doc.internal.pageSize.getWidth(), 30); // Adjust the position and size as needed
-        // Add footer
-        const footerWidth = (doc.internal.pageSize.getWidth() * 2) / 5; // Calculate the width of the footer image
-        const footerHeight = (footerWidth / 60) * 20; // Calculate the height based on the original aspect ratio
-        const footerX = doc.internal.pageSize.getWidth() - footerWidth; // Calculate X-coordinate
-        const footerY = doc.internal.pageSize.getHeight() - footerHeight; // Adjust the Y-coordinate as needed
-        doc.addImage(footerImage, 'JPEG', footerX, footerY, footerWidth, footerHeight); // Adjust the position and size as needed*/
+        doc.addImage(headerImage, 'JPEG', 0, 0, doc.internal.pageSize.getWidth(), 30);
+        const footerWidth = (doc.internal.pageSize.getWidth() * 2) / 5;
+        const footerHeight = (footerWidth / 60) * 20;
+        const footerX = doc.internal.pageSize.getWidth() - footerWidth;
+        const footerY = doc.internal.pageSize.getHeight() - footerHeight;
+        doc.addImage(footerImage, 'JPEG', footerX, footerY, footerWidth, footerHeight);
       }
     doc.save('document.pdf');
 }
--- src/utils/resuelvoUtils.js
+++ src/utils/resuelvoUtils.js
@@ -16,7 +16,7 @@ function listFiscal(arr){
 function listDefensa(arr){
     let aux = ''
     arr && arr.forEach((el,i) =>{
-        aux = `${aux}Defensa ${el.tipo}: ${el.nombre} ${(arr.length !== i+1) ? '\n' : ''}`
+        aux = `${aux}Defensa ${el.tipo}: ${el.nombre} ${el.imputado ? `(En representación de ${el.imputado})` : ''} ${(arr.length !== i+1) ? '\n' : ''}`
     })
     return aux
 }
@@ -30,7 +30,7 @@ function listImputado(arr){
 function listPartes(arr){
     let aux = ''
     arr && arr.forEach((el,i) =>{
-        aux = `${aux}${el.role}: ${el.name} ${(arr.length !== i+1) ? '\n' : ''}`
+        aux = `${aux}${el.role}: ${el.name} ${el.dni ? ` D.N.I. N.°:${el.dni}` : ''} ${(arr.length !== i+1) ? '\n' : ''}`
     })
     return aux
 }
@@ -52,6 +52,32 @@ Operador: ${item.operador}
 Fundamentos y Resolución: ${item.resuelvoText}`
     )
     return resuelvo
+}
+
+export function generateResuelvoSection(item, date){
+    const sections = []
+    sections.push({title: 'Lugar y Fecha', text: `San Juan, ${date.split('').splice(0,2).join('')} de ${capitalizeFirst(getMonthName(date.split('').splice(2,2).join('')))} de ${date.split('').splice(4,4).join('')}.`})
+    sections.push({title: 'Tipo de Audiencia', text: `${item.tipo}${item.tipo2 && ' - ' + item.tipo2}${item.tipo3 && ' - ' + item.tipo3}.`})
+    sections.push({title: 'Legajo', text: `N° ${item.numeroLeg} Caratulado "${item.caratula}".`})
+    sections.push({title: 'Sala de Audiencias', text: `${item.sala}.`})
+    sections.push({title: 'Hora programada', text: `${item.hora} horas.`})
+    sections.push({title: 'Hora real de inicio', text: `${item.hitos[0].split(' | ').splice(0,1)} horas.`})
+    if(item.juez.split('+').length > 1){
+        sections.push({title: 'Tribunal Colegiado', text: `${item.juez.split('+').map(j => capitalizeFirst(j.toLowerCase())).join('\n')}`})
+    }else{
+        sections.push({title: 'Juez', text: `${capitalizeFirst(item.juez.toLowerCase())}`})
+    }
+    listFiscal(item.mpf).split('\n').forEach(f => sections.push({title: `${f.split(':')[0]}`, text: `${f.split('-')[0]} UFI: ${f.split('-')[f.split('-').length - 1]}`}))
+    listDefensa(item.defensa).split('\n').forEach(d => sections.push({title: `${d.split(':')[0]}`, text: `${d.split(':')[1]}`}))
+    listImputado(item.imputado).split('\n').forEach(i => sections.push({title: `${i.split(':')[0]}`, text: `${i.split(':')[1]}`}))
+    listPartes(item.partes).split('\n').forEach(p => sections.push({title: `${p.split(':')[0]}`, text: `${p.split(':')[1]}`}))
+    sections.push({title: 'Operador:', text: `${item.operador}`})
+    return sections
+}
+export function generateMinutaSection(minuta){
+    const sections = []
+    minuta.split('\n').forEach(m => sections.push({text: `${m.split(')').join(')\n')}`}))
+    return sections
 }
 
 export function copyResuelvoToClipboard(item, date){
