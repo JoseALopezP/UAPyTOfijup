c0f142fd677eec487e6ff203ae66ba48e3e6ce66
José López <jalp42@live.com>
2024-06-27 09:33:21 -0300

    to merge

--- src/app/tablero/components/InformationSection.jsx
+++ src/app/tablero/components/InformationSection.jsx
@@ -9,11 +9,9 @@ export function InformationSection () {
     const { user } = useAuthContext();
     const router = useRouter();
     const [showInfo2, setShowInfo2] = useState(true);
-
     const tick = () => {
         setShowInfo2(prevShowInfo2 => !prevShowInfo2);
     }
-
     useEffect(() => {
         if (!user) router.push("/signin");
     }, [user, router]);
