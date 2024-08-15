77e4b4d2b4be0df31799c0bfba9e4c90426b00c6
José López <jalp42@live.com>
2024-08-15 13:29:36 -0300

    Add audiencia form getting in shape

--- src/app/audienciasUAC/components/AudienciaAddList.jsx
+++ src/app/audienciasUAC/components/AudienciaAddList.jsx
@@ -14,6 +14,7 @@ export function AudienciaAddList ({date}) {
     const { user } = useAuthContext()
     const router = useRouter()
     useEffect(() => {
+        
       if (user == null) router.push("/signin")
     }, [user])
     function tick() {
