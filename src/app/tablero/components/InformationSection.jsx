0d9991d229b5a5aa0aff7cfeacd679db4ccd7111
José López <jalp42@live.com>
2024-09-02 13:34:59 -0300

    Changes in process

--- src/app/tablero/components/InformationSection.jsx
+++ src/app/tablero/components/InformationSection.jsx
@@ -9,20 +9,6 @@ export function InformationSection () {
     const { user } = useAuthContext();
     const router = useRouter();
     const [showInfo2, setShowInfo2] = useState(true);
-    /*const tick = () => {
->>>>>>> e527ededb44a3bc4396de2111f01eb31d1cdcbd4
-        setShowInfo2(ShowInfo2 => !prevShowInfo2);
-    }
-    useEffect(() => {
-        if (!user) router.push("/signin");
-    }, [user, router]);
-
-    useEffect(() => {
-        const timerID = setInterval(tick, 30000);
-        return () => {
-            clearInterval(timerID);
-        };
-    }, []);*/
 
     return (
         <section className={`${styles.infoSection} ${showInfo2 ? styles.infoSectionNotShow : styles.infoSectionShow}`}>
