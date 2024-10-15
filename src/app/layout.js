d6417abe58737dab3dd147fa1c42a082b03d2a62
José López <jalp42@live.com>
2024-10-15 13:20:47 -0300

    New navBar style working

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import "./globals.css";
+import NavBar from "./navBar/NavBar";
 import { titillium } from "./ui/fonts";
 
 export const metadata = {
@@ -12,7 +13,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={titillium.className}>{children}</body>
+      <body className={titillium.className}>
+        <NavBar/>
+        {children}
+      </body>
     </html>
   );
 }
