f2e74a794fcf2448f4c5229f412ddcd65ae74ed3
JoseALopezP <jalp42@live.com>
2024-10-21 23:41:24 -0300

    creating utils for legajo input error checking

--- src/app/Agregar-Audiencia/components/AudienciaIndiv.jsx
+++ src/app/Agregar-Audiencia/components/AudienciaIndiv.jsx
@@ -15,7 +15,19 @@ export function AddAudienciaIndiv(element) {
     const [tipo3, setTipo3]= useState(element.tipo3 || '')
     const [juez, setJuez] = useState(element.juez || '')
 
-    
+    const handleLegChange = (value) => {
+        
+        if((value.split('-').join('').length == 3 || value.split('-').join('').length == 5) && !(value.length < legajo.length)){
+            setLegajo(value.toUpperCase() +'-')
+        }else{
+            if(legajo.length == 4 && (legajo[legajo.length-1] !== '-')){
+                setLegajo(legajo.split('').splice((legajo.length-1),0,'-').join(''))
+            }else{
+                setLegajo(value.toUpperCase())
+            }
+        }
+    }
+
     return(
         <form onClick={() => setShow(!show)} className={`${styles.tableRow}`}>
             <p className={`${styles.tableCell} ${styles.tableCellState}`}>●</p>
@@ -23,11 +35,13 @@ export function AddAudienciaIndiv(element) {
             <span className={`${styles.tableCell} ${styles.tableCellSala}`}>
                 <input list='sala' className={`${styles.salaSelect}`} onChange={e => setSala(e.target.value)}/>
                     <datalist id='sala' className={`${styles.salaSelect}`}><option>{sala}</option>
-                    {desplegables && desplegables.salas.map(el =>(
+                    {desplegables.salas && desplegables.salas.map(el =>(
                         <option key={el} value={el}>SALA {el}</option>
                     ))}</datalist>
             </span>
-            <span className={`${styles.tableCell} ${styles.tableCellLegajo}`}></span>
+            <span className={`${styles.tableCell} ${styles.tableCellLegajo}`}>
+                <input type='string' value={legajo} onChange={e => handleLegChange(e.target.value)}/>
+            </span>
             <span className={`${styles.tableCell} ${styles.tableCellTipo}`}></span>
             <span className={`${styles.tableCell} ${styles.tableCellJuez}`}></span>
             <span className={`${styles.tableCell} ${styles.tableCellAccion}`}><button>X</button></span>
