137537d267aa291d16d31b49d11e71623e547daf
José López <jalp42@live.com>
2025-02-04 08:12:47 -0300

    Solved unique child keys and svg tag names

--- src/app/Situacion-Corporal/components/SitCorporalList.jsx
+++ src/app/Situacion-Corporal/components/SitCorporalList.jsx
@@ -6,7 +6,7 @@ export default function SitCorporalList({list, date, legSearch}) {
     const [selected, setSelected] = useState(null)
     return (
         <div className={`${styles.sitListBlock}`}>{list.filter(el => el.numeroLeg.includes(legSearch)).map(el =>(
-            <SitCorporalIndiv aud={el} date={date} selected={selected} selectedF={setSelected}/>
+            <SitCorporalIndiv key={el.numeroLeg + el.hora} aud={el} date={date} selected={selected} selectedF={setSelected}/>
         ))}</div>
     )
 }
