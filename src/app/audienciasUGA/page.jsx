50776930c7697817df2306f3d67132cf0727ad31
José López <jalp42@live.com>
2025-02-13 13:30:45 -0300

    Finished development spree 13 02

--- src/app/audienciasUGA/page.jsx
+++ src/app/audienciasUGA/page.jsx
@@ -1,7 +1,5 @@
 'use client'
 import { DataContextProvider } from "@/context/DataContext";
-import { AudienciaList } from "../audienciasUAC/tablero/components/AudienciaList";
-
 export default function Admin() {
     /*useEffect(() => {
       if (user == null) router.push("/signin")
