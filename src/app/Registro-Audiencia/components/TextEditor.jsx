0e785088fa83505d6fa5efcfa5442e47c0ddc8b7
JoseALopezP <jalp42@live.com>
2025-03-06 17:42:03 -0300

    no nap

--- src/app/Registro-Audiencia/components/TextEditor.jsx
+++ src/app/Registro-Audiencia/components/TextEditor.jsx
@@ -3,16 +3,15 @@ import React, { useState, useEffect } from "react";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
 
-export default function TextEditor(textValue, setTextValue){
-  const [editorValue, setEditorValue] = useState("");
-  const handleChange = (value) => {
-    setValue(value);
+export default function TextEditor({ textValue, setTextValue }){
+  const handleChange = (textValue) => {
+    setTextValue(textValue);
   };
   return (
     <div>
       <h2>Simple Quill Editor</h2>
       <ReactQuill
-        value={value}
+        value={textValue}
         onChange={handleChange}
         modules={{
           toolbar: [
