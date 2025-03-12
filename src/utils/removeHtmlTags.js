5f8269da353b911c926d588444d49b1cc1017820
José López <jalp42@live.com>
2025-03-12 13:05:29 -0300

    Solved line jump issues

--- src/utils/removeHtmlTags.js
+++ src/utils/removeHtmlTags.js
@@ -1,6 +1,8 @@
-export const removeHtmlTags =(text)=> {
-    if (typeof text !== 'string') {
-      return '';
-    }
-    return text.replace(/<[^>]+>/g, '');
-  }
+export const removeHtmlTags = (html) => {
+  if (typeof html !== 'string') return '';
+  return html
+    .replace(/&nbsp;/g, ' ') 
+    .replace(/<br\s*\/?>/g, '\n')
+    .replace(/<\/p>/g, '\n')
+    .replace(/<[^>]+>/g, '');
+};
