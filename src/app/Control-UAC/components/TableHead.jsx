369f7084f4d5fce3671f1b1ba6d98426b77add60
José López <jalp42@live.com>
2025-04-08 18:37:37 -0300

    Added inputs date

--- src/app/Control-UAC/components/TableHead.jsx
+++ src/app/Control-UAC/components/TableHead.jsx
@@ -3,16 +3,16 @@ import styles from '../ControlUac.module.css'
 export default function TableHead(){
     return (
         <div className={`${styles.tableHeadControlUac}`}>
-            <span className={`${styles.tableHeadCell}`}>
-                <input type='text' className={`${styles.inputday}`}/>
-                <input type='text' className={`${styles.inputmonth}`}/>
-                <input type='text' className={`${styles.inputyear}`}/>
+            <span className={`${styles.tableHeadCell} ${styles.tableHeadCellDate}`}>
+                <input type='text' maxLength={2} className={`${styles.inputday}`}/>
+                <input type='text' maxLength={2} className={`${styles.inputmonth}`}/>
+                <input type='text' maxLength={4} className={`${styles.inputyear}`}/>
             </span>
-            <span className={`${styles.tableHeadCell}`}>LEGAJO</span>
-            <span className={`${styles.tableHeadCell}`}>TIPO</span>
-            <span className={`${styles.tableHeadCell}`}>JUEZ</span>
-            <span className={`${styles.tableHeadCell}`}>RESULTADO</span>
-            <span className={`${styles.tableHeadCell}`}>COMENTARIO</span>
+            <span className={`${styles.tableHeadCell} ${styles.tableHeadCellLeg}`}>LEGAJO</span>
+            <span className={`${styles.tableHeadCell} ${styles.tableHeadCellTipo}`}>TIPO</span>
+            <span className={`${styles.tableHeadCell} ${styles.tableHeadCellJuez}`}>JUEZ</span>
+            <span className={`${styles.tableHeadCell} ${styles.tableHeadCellRes}`}>RESULTADO</span>
+            <span className={`${styles.tableHeadCell} ${styles.tableHeadCellCom}`}>COMENTARIO</span>
         </div>
     )
 }
