86959e92531a73ccc377f413190ea6de423fdea3
José López <jalp42@live.com>
2025-04-10 13:19:52 -0300

    Table work ing progress

--- src/app/Control-UAC/components/DateSelector.jsx
+++
@@ -1,9 +0,0 @@
-import styles from '../ControlUac.module.css'
-
-export default function DateSelector(){
-    return (
-        <div className={`${styles.dateSelectorBlock}`}>
-
-        </div>
-    )
-}
--- src/app/Control-UAC/components/tableIndiv.jsx
+++ src/app/Control-UAC/components/tableIndiv.jsx
@@ -4,12 +4,18 @@ export default function TableIndiv({item}){
 
     return (
         <div className={`${styles.tableRowControlUac}`}>
-            <span className={`${styles.tableCell} ${styles.horaCell}`}>{item.hora}</span>
-            <span className={`${styles.tableCell} ${styles.numeroLegCell}`}>{item.numeroLeg}</span>
-            <span className={`${styles.tableCell} ${styles.tipoCell}`}>{item.tipo + ' ' + item.tipo2 + ' ' + item.tipo3}</span>
-            <span className={`${styles.tableCell} ${styles.juezCell}`}>{item.juez.split('+').map(el => el.split(' ').splice(0,2)).join(' ')}</span>
-            <span className={`${styles.tableCell} ${styles.resultCell}`}>{item.resultado && item.resultado}</span>
-            <span className={`${styles.tableCell} ${styles.CommentCell}`}>{item.comentario && item.comentario}</span>
+            <div className={`${styles.tableCell} ${styles.horaCell}`}><p className={`${styles.horaCellP}`}>
+                {item.hora}</p></div>
+            <div className={`${styles.tableCell} ${styles.numeroLegCell}`}><p className={`${styles.numeroLegCellP}`}>
+                {item.numeroLeg}</p></div>
+            <div className={`${styles.tableCell} ${styles.tipoCell}`}><p className={`${styles.tipoCellP}`}>
+                {item.tipo + ' ' + item.tipo2 + ' ' + item.tipo3}</p></div>
+            <div className={`${styles.tableCell} ${styles.juezCell}`}><p className={`${styles.juezCellP}`}>
+                {item.juez.split('+').map(el => el.split(' ').slice(1,3).join(' ')).join(', ')}</p></div>
+            <div className={`${styles.tableCell} ${styles.resultCell}`}><p className={`${styles.resultCellP}`}>
+                {item.resultado && item.resultado}</p></div>
+            <div className={`${styles.tableCell} ${styles.commentCell}`}><p className={`${styles.commentCellP}`}>
+                {item.comentario && item.comentario}</p></div>
         </div>
     )
 }
