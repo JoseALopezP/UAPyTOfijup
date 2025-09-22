6a0769902caed7c3609f7a67d18e0964c1ec5543
Adrian Newey <jalp42@live.com>
2025-09-22 13:31:57 -0300

    Working with editing bloque sindividiaully

--- src/app/Carga-Juicio/components/BloqueJuicio.jsx
+++ src/app/Carga-Juicio/components/BloqueJuicio.jsx
@@ -5,11 +5,16 @@ export function BloqueJuicio ({bloque}){
     const [fechaD, setFechaD] = useState(bloque.fechaD)
     const [fechaM, setFechaM] = useState(bloque.fechaM)
     const [fechaA, setFechaA] = useState(bloque.fechaA)
+    const [horaH, setHoraH] = useState(bloque.fechaA)
+    const [horaM, setHoraM] = useState(bloque.fechaA)
     return(
         <div className={`${styles.bloqueJuicioSection}`}>
-            <p>{fechaD}{fechaM}{fechaA}</p>
-            <p>{bloque.hora}</p>
-            <p>{bloque.minuto}</p>
+            <input type='text' value={fechaD} onChange={e => setFechaD(e.target.value)}/>
+            <input value={fechaM} onChange={e => setFechaM(e.target.value)}/>
+            <input value={fechaA} onChange={e => setFechaA(e.target.value)}/>
+
+            <input value={horaH} onChange={e => setHoraH(e.target.value)}/>
+            <input value={horaM} onChange={e => setHoraM(e.target.value)}/>
         </div>
     )
 }
--- src/app/Carga-Juicio/components/BloqueList.jsx
+++ src/app/Carga-Juicio/components/BloqueList.jsx
@@ -4,6 +4,13 @@ import styles from './Carga-Juicio.module.css'
 export function BloqueList ({array=[{fechaD: '21',fechaM: '09',fechaA: '2025', hora:'16', minuto:'00'}]}){
     return(
         <section className={`${styles.bloqueListSection}`}>
+            <span className={`${styles.timeSelectorBar}`}>
+                <select>
+                    <label>Seleccionar horario</label>
+                    <option value={'16'}>8</option>
+                    <option value={'16'}>16</option>
+                </select>
+            </span>
             {array && array.map(el => (
                 <BloqueJuicio bloque={el} />
             ))}
