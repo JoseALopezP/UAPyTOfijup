c45076280468f8a659e6c97262c715914b74774d
José López <jalp42@live.com>
2026-04-23 07:59:31 -0300

    Solved juez naming

--- src/app/Sorteo-Operador/components/ListIndiv.jsx
+++ src/app/Sorteo-Operador/components/ListIndiv.jsx
@@ -5,30 +5,30 @@ import { nameTranslate } from '@/utils/traductorNombres';
 import { useContext, useEffect, useState } from 'react';
 import { todayFunction } from '@/utils/dateUtils';
 
-export default function ListIndiv({item}) {
-    const {desplegables, updateData} = useContext(DataContext)
+export default function ListIndiv({ item }) {
+    const { desplegables, updateData } = useContext(DataContext)
     const [operadorAud, setOperadorAud] = useState(item.operador || '')
-    const handleOperadorChange = (value) =>{
+    const handleOperadorChange = (value) => {
         updateData(todayFunction(), item.id, 'operador', value);
         setOperadorAud(value)
     }
-    useEffect(()=>{
+    useEffect(() => {
         setOperadorAud(item.operador)
     }, [item.operador])
     return (
         <div className={styles.listIndivBlock}>
-            <select onChange={(e)=>{handleOperadorChange(e.target.value)}} className={styles.operadorItem}>
+            <select onChange={(e) => { handleOperadorChange(e.target.value) }} className={styles.operadorItem}>
                 <option>{nameTranslate(operadorAud)}</option>
                 <option value={''}></option>
-                {desplegables.operador && desplegables.operador.map(el =>(
+                {desplegables.operador && desplegables.operador.map(el => (
                     <option key={el} value={el}>{nameTranslate(el)}</option>
                 ))}
             </select>
             <p className={styles.horaItem}>{item.hora}</p>
             <p className={styles.legajoItem}>{item.numeroLeg.split('-SJ-')[1]}</p>
             <p className={styles.tipoItem}>{item.tipo}</p>
-            <p className={styles.juezItem}>{item.juez ? item.juez.split('+').map(el=>el.split(' ').splice(1,3).join(' ')).join(' ') : "NA"}</p>
-            <p className={styles.demoraItem}>{demoraCalculator(item.tipo+item.tipo2+item.tipo3)}</p>
+            <p className={styles.juezItem}>{item.juez ? item.juez.split('+').map(el => el.split(' ').splice(0, 3).join(' ')).join(' ') : "NA"}</p>
+            <p className={styles.demoraItem}>{demoraCalculator(item.tipo + item.tipo2 + item.tipo3)}</p>
         </div>
     );
 }
